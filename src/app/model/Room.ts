80100f84ed6b0b9d6f90b111f3c9437cb22b2eb1
Mateusz Bajdak <m.bajdak@samsung.com>
2019-11-25 12:02:51 +0100

    adapt to new api

--- src/app/model/Room.ts
+++ src/app/model/Room.ts
@@ -1,6 +1,6 @@
 export class Room {
     constructor(
-        public id: string, public name: string, public owner: string, 
+        public roomId: string, public roomName: string, 
         public signDeadline: number, public postDeadline: number, public priorityDeadline: number
         ) { }
 }
