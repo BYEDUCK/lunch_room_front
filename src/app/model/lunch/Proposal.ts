549172f913f68c691909aa2c3d2bc2aa68abceae
Mateusz Bajdak <m.bajdak@samsung.com>
2020-01-09 16:03:13 +0100

    add proposal owner id to proposal

--- src/app/model/lunch/Proposal.ts
+++ src/app/model/lunch/Proposal.ts
@@ -2,8 +2,8 @@ import { MenuItem } from './MenuItem';
 
 export class Proposal {
     constructor(
-        public proposalId: string, public title: string, public menuUrl: string,
-        public menuItems: MenuItem[], public ratingSum: number, public votesCount: number
+        public proposalId: string, public title: string, public menuUrl: string, public menuItems: MenuItem[],
+        public proposalOwnerId: string, public ratingSum: number, public votesCount: number
     ) {
     }
 }
