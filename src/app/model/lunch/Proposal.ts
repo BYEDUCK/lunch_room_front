7cd54d496d3b13952700f9e0219ff9502f9cdd6a
Mateusz Bajdak <m.bajdak@samsung.com>
2020-01-10 12:38:33 +0100

    prod build

--- src/app/model/lunch/Proposal.ts
+++ src/app/model/lunch/Proposal.ts
@@ -2,8 +2,6 @@ import { MenuItem } from './MenuItem';
 
 export class Proposal {
 
-    public isDeleted = false;
-
     constructor(
         public proposalId: string, public title: string, public menuUrl: string, public menuItems: MenuItem[],
         public proposalOwnerId: string, public ratingSum: number, public votesCount: number
