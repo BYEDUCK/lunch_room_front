b1cca0fa7df88a917cef72ff001c8da0927eca76
BYEDUCK <mateuszbajdak@gmail.com>
2020-01-20 23:33:12 +0100

    client id fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   serverUrl: 'https://lunchroom-byeduck.herokuapp.com',
   serverWsUrl: 'wss://lunchroom-byeduck.herokuapp.com',
   googleOAuthUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
-  googleOAuthClientId: '32940535387-8hi26d85u0lboarf07i7cmv8e4djf144.apps.googleusercontent.com',
+  googleOAuthClientId: '32940535387-q270au7repg5hvof8irb2r7ennip9fej.apps.googleusercontent.com',
   googleOAuthRedirectUri: 'https://byeduck.github.io/lunch_room_front/lunch_room_front/signIn'
 };
