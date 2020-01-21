cc878dd522512acb5692633f430e641c2d9fdae8
Mateusz Bajdak <m.bajdak@samsung.com>
2020-01-21 11:51:34 +0100

    switch to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  environment: 'https://byeduck.github.io',
+  domain: 'https://lunchroom-2f3dd.firebaseapp.com',
   serverUrl: 'https://lunchroom-byeduck.herokuapp.com',
   serverWsUrl: 'wss://lunchroom-byeduck.herokuapp.com',
   googleOAuthUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
   googleOAuthClientId: '32940535387-q270au7repg5hvof8irb2r7ennip9fej.apps.googleusercontent.com',
-  googleOAuthRedirectUri: 'https://byeduck.github.io/lunch_room_front/lunch_room_front/signIn'
+  googleOAuthRedirectUri: 'https://lunchroom-2f3dd.firebaseapp.com/signIn'
 };
