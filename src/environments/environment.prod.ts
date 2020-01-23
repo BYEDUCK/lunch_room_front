ee5a6d4bf48bfa317b2292b3f3adff1ab170df8f
Mateusz Bajdak <m.bajdak@samsung.com>
2020-01-23 10:06:40 +0100

    update google auth redirect uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   serverWsUrl: 'ws://vps778858.ovh.net:8080',
   googleOAuthUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
   googleOAuthClientId: '32940535387-q270au7repg5hvof8irb2r7ennip9fej.apps.googleusercontent.com',
-  googleOAuthRedirectUri: 'http://vps778858.ovh.net/lunchroom/signIn'
+  googleOAuthRedirectUri: 'http://www.byeduck.com/lunchroom/signIn'
 };
