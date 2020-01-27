3435f38c9581d8600a9dd756e96076dc7e5a75e9
Mateusz Bajdak <m.bajdak@samsung.com>
2020-01-27 12:27:44 +0100

    added https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  serverUrl: 'http://vps778858.ovh.net:8080',
-  serverWsUrl: 'ws://vps778858.ovh.net:8080',
+  serverUrl: 'https://byeduck.com:8080',
+  serverWsUrl: 'wss://byeduck.com:8080',
   googleOAuthUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
   googleOAuthClientId: '32940535387-q270au7repg5hvof8irb2r7ennip9fej.apps.googleusercontent.com',
-  googleOAuthRedirectUri: 'http://www.byeduck.com/lunchroom/signIn'
+  googleOAuthRedirectUri: 'https://www.byeduck.com/lunchroom/signIn'
 };
