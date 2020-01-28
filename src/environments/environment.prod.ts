5a6c73ab9469bb267061fdf6b27377ea52609ac6
Mateusz Bajdak <m.bajdak@samsung.com>
2020-01-28 10:32:37 +0100

    fix redirect uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   serverWsUrl: 'wss://byeduck.com:8080',
   googleOAuthUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
   googleOAuthClientId: '32940535387-q270au7repg5hvof8irb2r7ennip9fej.apps.googleusercontent.com',
-  googleOAuthRedirectUri: 'https://www.byeduck.com/lunchroom/signIn'
+  googleOAuthRedirectUri: 'https://byeduck.com/lunchroom/signIn'
 };
