49864dacbddd6bab6fb3ef52e6b6abc8ab79dc72
BYEDUCK <mateuszbajdak@gmail.com>
2020-02-28 20:50:01 +0100

    change backend path for dev env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   domain: 'localhost',
-  serverUrl: 'http://localhost:48080',
-  serverWsUrl: 'ws://localhost:48080',
+  serverUrl: 'http://localhost:48080/api/lunchroom',
+  serverWsUrl: 'ws://localhost:48080/api/lunchroom',
   googleOAuthUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
   googleOAuthClientId: '32940535387-q270au7repg5hvof8irb2r7ennip9fej.apps.googleusercontent.com',
   googleOAuthRedirectUri: 'http://localhost:4200/signIn',
