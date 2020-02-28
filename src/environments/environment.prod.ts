6929fe4af71998a8c869dc59aa523d085e2b6fb6
BYEDUCK <mateuszbajdak@gmail.com>
2020-02-28 19:50:33 +0100

    change backend url for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   domain: 'byeduck.com',
-  serverUrl: 'https://byeduck.com:8080',
-  serverWsUrl: 'wss://byeduck.com:8080',
+  serverUrl: 'https://byeduck.com/api/lunchroom',
+  serverWsUrl: 'wss://byeduck.com/api/lunchroom',
   googleOAuthUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
   googleOAuthClientId: '32940535387-q270au7repg5hvof8irb2r7ennip9fej.apps.googleusercontent.com',
   googleOAuthRedirectUri: 'https://byeduck.com/lunchroom/signIn',
